8e0d8a50b77adefd3642db6942372dcada692f85
Taeyong Jeon <taeyongjeon24@gmail.com>
2022-12-25 23:34:37 +0900

    취득일 제목 디자인 변경

--- lib/capital_gain/pages/components/conditional_date.dart
+++ lib/capital_gain/pages/components/conditional_date.dart
@@ -1,20 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:taxai/capital_gain/constants/first_filter.dart';
 import 'package:taxai/capital_gain/functions/check_condition.dart';
 import 'package:taxai/common_widgets/custom_datepicker.dart';
 import 'package:taxai/common_widgets/custom_oxdropdown.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:taxai/components/colorbase.dart';
+import 'package:taxai/components/typography.dart';
 
 class ConditionalDate extends StatelessWidget {
+  final int index;
+
+  // ignore: prefer_typing_uninitialized_variables
+  final controller;
   const ConditionalDate({
     Key? key,
     required this.index,
     required this.controller,
   }) : super(key: key);
-
-  final int index;
-  // ignore: prefer_typing_uninitialized_variables
-  final controller;
 
   @override
   Widget build(BuildContext context) {
@@ -60,17 +63,21 @@ class ConditionalDate extends StatelessWidget {
       List<Widget> widgets = [];
       widgets.add(Divider());
 
-      widgets.add(FractionallySizedBox(
-        widthFactor: 0.7,
+      widgets.add(ResponsiveRowColumnItem(
         child: Padding(
           padding: const EdgeInsets.only(top: 30.0, bottom: 30.0),
-          child: Text("2. 취득일 등 추가 정보",
+          child: Container(
+            alignment: Alignment.center,
+            decoration:
+                BoxDecoration(border: Border.all(width: 1, color: borderColor)),
+            child: Text(
+              "3. 취득일 등 추가정보",
               textAlign: TextAlign.left,
-              style: Theme.of(context).textTheme.headline4?.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(200, 0, 0, 0))),
+              style: headlineTextStyle,
+            ),
+          ),
         ),
-      )); //
+      ));
 
       for (Map widgetItem in additionalInfo) {
         bool checkedResult = checkCondition(index, widgetItem["condition"]);
